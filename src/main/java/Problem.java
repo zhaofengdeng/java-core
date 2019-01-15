8b5f337855464d8cb6b8ba5d694e32509bedbc2e
zhaofengdeng <zhao_fengdeng@khktsoft.com>
2019-01-15 09:26:07 +0800

    泛型学习

---
+++ src/main/java/Problem.java
@@ -0,0 +1,4 @@
+
+public class Problem {
+//泛型使用列表优先于数组
+}
---
+++ src/main/java/java/JavaCoreOther.java
@@ -0,0 +1,12 @@
+package java;
+//消除警告，不建议使用，使用必须证明该方法或者改类是安全的
+//可以使用在任何粒度的级别上
+@SuppressWarnings("unckecked")
+public class JavaCoreOther {
+	/**
+	 * 第一点：
+	 * 消除警告，不建议使用，使用必须证明该方法或者改类是安全的
+	 * 可以使用在任何粒度的级别上
+	 * @SuppressWarnings("unckecked")
+	 */
+}
